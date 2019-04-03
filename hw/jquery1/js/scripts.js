56c839dffe2e6a0a095e33f96894420622b56e94
andrianagibson <amg493@miami.edu>
2019-04-02 22:20:36 -0400

    idk

--- hw/jquery1/js/scripts.js
+++ hw/jquery1/js/scripts.js
@@ -1,69 +1,68 @@
-
-var step=0
-var titles=['prepare ingredients',//thi is 0 titles[0]
-'make the layers',//this is 1 titles[1]
-'bake!',//this is 2 titles[2]
-'They are done! What now?',//this is 3 titles[3]
-'Questions? Answered.'//this is 4 titles[4]
+var step = 0
+var titles = ['prepare ingredients', //thi is 0 titles[0]
+  'make the layers', //this is 1 titles[1]
+  'bake!', //this is 2 titles[2]
+  'They are done! What now?', //this is 3 titles[3]
+  'Questions? Answered.' //this is 4 titles[4]
 ];
 
 
-$(function(){
+$(function() {
   $("#stepinfo, section, footer, .prev").hide();
 });
 
-$('.start') .on('click',function(){
+$('.start').on('click', function() {
   //do stuff when button is clicked//
   $('.intro').hide();
   $('footer,#stepinfo').show();
   step = 1;
-  $('#step'+step).show();
+  $('#step' + step).show();
   //f(x)=x+2
   //set the title for our current step
-  $('#stepinfo').text('step '+step+': '+titles[step-1]);
+  $('#stepinfo').text('step ' + step + ': ' + titles[step - 1]);
 });
 
-$('.next').on('click',function(){
-//make the change only if we are under the number of steps
-  $('#step'+step).hide();
-step++;
-if(step == 2) {
-  $('.prev').show();
-}
-if(step == 5){
-  $('.next').hide();
-}
-  if(step <= 5) {
-  $('#step'+step).show();
-  $('#stepinfo').text('Step '+step+': '+titles[step-1]);
-} else {
-  step--;
-  $('#step'+step).show();
+$('.next').on('click', function() {
+  //make the change only if we are under the number of steps
+  $('#step' + step).hide();
+  step++;
+  if (step == 2) {
+    $('.prev').show();
+  }
+  if (step == 5) {
+    $('.next').hide();
+  }
+  if (step <= 5) {
+    $('#step' + step).show();
+    $('#stepinfo').text('Step ' + step + ': ' + titles[step - 1]);
+  } else {
+    step--;
+    $('#step' + step).show();
 
-}
+  }
 });
 
 //previous
-$('.prev').on('click',function(){
-//make the change only if we are under the number of steps
-  $('#step'+step).hide();
-step--;
-if(step == 1) {
-  $('.prev').hide();
-} else {
-  $('.next').show();
-}
-if(step == 5){
-  $('.next').hide();
-}
-  if(step >= 1) {
-  $('#step'+step).show();
-  $('#stepinfo').text('Step '+step+': '+titles[step-1]);
-} else {
+$('.prev').on('click', function() {
+  //make the change only if we are under the number of steps
+  $('#step' + step).hide();
   step--;
-  $('#step'+step).show();
+  if (step == 1) {
+    $('.prev').hide();
+  } else {
+    $('.next').show();
+  }
+  if (step == 5) {
+    $('.next').hide();
+  }
+  if (step >= 1) {
+    $('#step' + step).show();
+    $('#stepinfo').text('Step ' + step + ': ' + titles[step - 1]);
+  } else {
+    step--;
+    $('#step' + step).show();
 
-}
+  }
 });
 
 $('.start').on('click', function() {
